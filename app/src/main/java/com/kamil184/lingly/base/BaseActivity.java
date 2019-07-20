3a4360df58dc83b49ad80c2d9864f4b407376cf4
kamil184 <45402327+kamil184@users.noreply.github.com>
2019-07-20 11:15:38 +0300

    Constants, BaseActivity, но не до конца доделанный

---
+++ app/src/main/java/com/kamil184/lingly/base/BaseActivity.java
@@ -0,0 +1,142 @@
+package com.kamil184.lingly.base;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.kamil184.lingly.Constants;
+import com.kamil184.lingly.R;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    public final String TAG = this.getClass().getSimpleName();
+    public ProgressDialog progressDialog;
+    public ActionBar actionBar;
+    private long backPressedTime;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        actionBar = getSupportActionBar();
+
+    }
+
+    public void showProgress() {
+        showProgress(R.string.loading);
+    }
+
+    public void showProgress(@StringRes int message) {
+        hideProgress();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getString(message));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
+    public void hideProgress() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
+
+    public void hideKeyboard() {
+        // Check if no view has focus:
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public void showSnackBar(@StringRes int messageId) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                messageId, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    public void showSnackBar(View view, @StringRes int messageId) {
+        Snackbar snackbar = Snackbar.make(view, messageId, Snackbar.LENGTH_LONG);
+        snackbar.show();
+    }
+
+    public void showWarningDialog(int messageId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(messageId);
+        builder.setPositiveButton(R.string.button_ok, null);
+        builder.show();
+    }
+
+    public void showWarningDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setPositiveButton(R.string.button_ok, null);
+        builder.show();
+    }
+
+    public void showNotCancelableWarningDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setPositiveButton(R.string.button_ok, null);
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    public void showWarningDialog(int message, DialogInterface.OnClickListener listener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setPositiveButton(R.string.button_ok, listener);
+        builder.show();
+    }
+
+    public void showWarningDialog(String message, DialogInterface.OnClickListener listener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setPositiveButton(R.string.button_ok, listener);
+        builder.show();
+    }
+
+    //public boolean hasInternetConnection() { }
+
+    //public boolean checkAuthorization() { }
+
+    public void attemptToExitIfRoot() {
+        attemptToExitIfRoot(null);
+    }
+
+    public void attemptToExitIfRoot(@Nullable View anchorView) {
+        if (isTaskRoot()) {
+            if (backPressedTime + Constants.General.DOUBLE_CLICK_TO_EXIT_INTERVAL > System.currentTimeMillis()) {
+                super.onBackPressed();
+            } else {
+                if (anchorView != null) {
+                    showSnackBar(anchorView, R.string.press_once_again_to_exit);
+                } else {
+                    showSnackBar(R.string.press_once_again_to_exit);
+                }
+
+                backPressedTime = System.currentTimeMillis();
+            }
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+        }
+        return (super.onOptionsItemSelected(menuItem));
+    }
+}
